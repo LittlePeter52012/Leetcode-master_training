cea50b139bf51bb299e66481ebfef7b6305f9d8c
LittlePeter52012 <94422715+LittlePeter52012@users.noreply.github.com>
2022-03-17 13:07:29 +0800

    finished leetcode_master linked_list

---
+++ linked_list/Linked_list.cpp
@@ -0,0 +1,10 @@
+// 单链表
+struct ListNode {
+    int val;  // 节点上存储的元素
+    ListNode *next;  // 指向下一个节点的指针
+    ListNode(int x) : val(x), next(NULL) {}  // 节点的构造函数
+};
+
+ListNode* head = new ListNode(5);
+ListNode* head = new ListNode();
+head -> val = 5
